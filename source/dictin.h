ad2b2ce792d0ef4152a8ebe5b88c2e320ee7f0be
Ludmila Marian <ludmila.marian@gmail.com>
2021-04-23 15:21:13 +0200

    Adds header with attribution and license.

--- source/dictin.h
+++ source/dictin.h
@@ -1,3 +1,12 @@
+C This file is part of PREPRO.
+C
+C    Author: Dermott (Red) Cullen
+C Copyright: (C) International Atomic Energy Agency
+C
+C PREPRO is free software; you can redistribute it and/or modify it
+C under the terms of the MIT License; see LICENSE file for more details.
+
+
 C=======================================================================
 C
 C     DICTIN COMMON
