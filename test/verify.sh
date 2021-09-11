96d4ab2b1f5ebd3b135772de1a4fee8f8fd41c30
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-09-11 14:13:55 +0200

    improve paths in verify.sh

--- test/verify.sh
+++ test/verify.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
-PATH="$(pwd)/../../bin:$PATH"
+PATH="$(pwd)/../bin:$PATH"
 
 mkdir -p output
 cp input/* output
 cd output
 
-../../bin/endf2c
-../../bin/merger
-../../bin/linear
-../../bin/recent
+endf2c
+merger
+linear
+recent
 rm LINEAR.OUT
-../../bin/sigma1
+sigma1
 rm RECENT.OUT
-../../bin/activate
+activate
 rm SIGMA1.OUT
-../../bin/legend
+legend
 rm ACTIVATE.OUT
-../../bin/fixup
+fixup
 rm LEGEND.OUT
-../../bin/dictin
-../../bin/sixpak
-../../bin/spectra
-../../bin/groupie
-../../bin/mixer
-../../bin/virgin
-../../bin/evalplot
-../../bin/complot
-../../bin/evalhard1
+dictin
+sixpak
+spectra
+groupie
+mixer
+virgin
+evalplot
+complot
+evalhard1
 mv PLOT0001.ps evalhard1.ps
-../../bin/comhard1
+comhard1
 mv PLOT0001.ps comhard1.ps
