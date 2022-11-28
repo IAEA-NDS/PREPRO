b70c0c747ea3edb480169102823854efac7962e2
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-11-28 15:19:34 +0100

    use modified fixup version for acemaker

--- source/fixup.f
+++ source/fixup.f
@@ -1,12 +1,3 @@
-C This file is part of PREPRO.
-C
-C    Author: Dermott (Red) Cullen
-C Copyright: (C) International Atomic Energy Agency
-C
-C PREPRO is free software; you can redistribute it and/or modify it
-C under the terms of the MIT License; see LICENSE file for more details.
-
-
 C=======================================================================
 C
 C     PROGRAM FIXUP
@@ -3608,6 +3599,10 @@ C-----INITIALIZE LAST POINT TO ZERO ENERGY AND CROSS SECTION.
 C-----DEFINE FIRST POINT AT OR ABOVE THRESHOLD.
       XANOW=XA(I2A)
       YANOW=YA(I2A)
+cdla
+cdla   return to save first point
+cdla
+      return
 c-----------------------------------------------------------------------
 C
 C     2019/3/6 - THRESHOLD REACTION TREATMENT NO LONGER ALLOWED
